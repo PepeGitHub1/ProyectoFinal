b42daaec81b1b86445958afd5b14469e1f986cef
PepeGitHub1 <110578123+PepeGitHub1@users.noreply.github.com>
2022-08-18 14:22:55 -0500

    Movimiento Player1

--- ProyectoFinal/Assets/Scripts/MovePlayerForce.cs
+++ ProyectoFinal/Assets/Scripts/MovePlayerForce.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MovePlayerForce : MonoBehaviour
 {
+    [SerializeField]
+    [Range(5f, 2000f)] private float speed = 5f;
+
+    private Rigidbody playerRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(inputHorizontal, 0, inputVertical);
+
+        playerRB.AddForce(direction * speed, ForceMode.Force);
     }
 }
