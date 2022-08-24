129776847cacd595fa1f5f6bf90ae3f6252f2df3
PepeGitHub1 <110578123+PepeGitHub1@users.noreply.github.com>
2022-08-24 14:30:54 -0500

    actualizacion

--- ProyectoFinal/Assets/Scripts/InstanceRock.cs
+++ ProyectoFinal/Assets/Scripts/InstanceRock.cs
@@ -12,7 +12,7 @@ public class InstanceRock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("RockIntantiate", 2f, 0.5f);
+        InvokeRepeating("RockIntantiate", 2f, 1f);
     }
 
     // Update is called once per frame
